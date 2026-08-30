9cea6869bea92e1be9e5303f0712e9d7df1ecaea
santoshnishad2612008-hub <santoshnishad2612008@gmail.com>
2026-08-30 12:30:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+// CORS
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
@@ -13,7 +14,7 @@ app.use((req, res, next) => {
 
   res.header(
     "Access-Control-Allow-Methods",
-    "GET,POST,OPTIONS"
+    "GET, POST, OPTIONS"
   );
 
   res.header(
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
   }
 
   next();
+
 });
 app.use(express.static(path.join(__dirname, "public")));
 
